635c12e4b14efad8ed54b74b1325f678f7fabbea
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2020-01-14 12:23:39 -0500

    1

--- src/exercises.js
+++ src/exercises.js
@@ -2,7 +2,22 @@ function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
   }
+  if (a[0] ===b[0]) {
 
+   return true;
+
+ }
+
+ else if (a[a.length-1] === b[b.length-1]) {
+
+   return true;
+
+ }
+
+ else {
+
+   return false;
+ }
   // write your code here
 }
 
